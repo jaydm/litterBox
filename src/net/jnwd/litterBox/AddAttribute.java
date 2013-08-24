33698fd80f8ad19863c731851e77f077a7f90db9
Jay D. McHugh <jaydmchugh@gmail.com>
2013-08-24 18:54:37 -0500

    Beginning to add the framework for adding classes and attributes.

---
+++ src/net/jnwd/litterBox/AddAttribute.java
@@ -0,0 +1,22 @@
+package net.jnwd.litterBox;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class AddAttribute extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_attribute);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.add_attribute, menu);
+		return true;
+	}
+
+}
