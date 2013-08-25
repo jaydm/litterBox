474099d51c0a4c396da6347d944fe8cc8eaae4ce
Jay D. McHugh <jaydmchugh@gmail.com>
2013-08-24 19:05:35 -0500

    Moving the UI classes to where they belong.

--- src/net/jnwd/litterBox/ui/AddAttribute.java
+++ src/net/jnwd/litterBox/ui/AddAttribute.java
@@ -1,21 +1,24 @@
-package net.jnwd.litterBox;
+package net.jnwd.litterBox.ui;
 
+import net.jnwd.litterBox.R;
+import net.jnwd.litterBox.R.layout;
+import net.jnwd.litterBox.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class AddClass extends Activity {
+public class AddAttribute extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_add_class);
+		setContentView(R.layout.activity_add_attribute);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.add_class, menu);
+		getMenuInflater().inflate(R.menu.add_attribute, menu);
 		return true;
 	}
 
--- src/net/jnwd/litterBox/ui/AddClass.java
+++ src/net/jnwd/litterBox/ui/AddClass.java
@@ -1,21 +1,24 @@
-package net.jnwd.litterBox;
+package net.jnwd.litterBox.ui;
 
+import net.jnwd.litterBox.R;
+import net.jnwd.litterBox.R.layout;
+import net.jnwd.litterBox.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class AddAttribute extends Activity {
+public class AddClass extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_add_attribute);
+		setContentView(R.layout.activity_add_class);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.add_attribute, menu);
+		getMenuInflater().inflate(R.menu.add_class, menu);
 		return true;
 	}
 
